44bd9ceba7fdbfa96ad6b87e00e8057fd9c7ff88
ItaloFL <grd788@gmail.com>
2025-04-03 15:57:28 -0500

    feat :sparkles: upload multer path resolve

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -2,7 +2,7 @@ import multer from 'multer'
 import path from 'path'
 import crypto from 'crypto'
 
-const dest = path.resolve(__dirname, '..', '..', 'tmp', 'uploads')
+const dest = path.resolve(__dirname, '..', '..', 'uploads')
 
 export default {
   dest,
